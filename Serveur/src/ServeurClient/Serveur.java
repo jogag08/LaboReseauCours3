1eff71675d29354b9afcd0d9662d1ef96632bf45
unknown <jogag08@gmail.com>
2022-09-21 01:07:55 -0400

    CreateFiles kinda work

--- Serveur/src/ServeurClient/Serveur.java
+++ Serveur/src/ServeurClient/Serveur.java
@@ -2,8 +2,11 @@ package ServeurClient;
 
 import java.net.*;
 import java.io.*;
+import java.io.File;
 
 public class Serveur {
+
+    private String protocol = "jaune";
     private Socket socket = null;
 
     private ServerSocket server = null; //Permet d'ouvrir la connection, d'écouter si quelqu'un veut se connecter et
@@ -16,15 +19,42 @@ public class Serveur {
             server = new ServerSocket(port);
             System.out.println("Waiting for connection");
             socket = server.accept();
+            System.out.println("Connecté");
 
             in = new DataInputStream((socket.getInputStream()));
 
             String line = "";
+            String[] listFiles;
 
             while(!line.equals("Fini")){
                 try{
                     line= in.readUTF();
                     System.out.println(line);
+                    if(CheckProtocol(line))
+                    {
+                        String command = CheckCommand(line);
+
+                        if(command.equals("CreateFile"))
+                        {
+                            File newF = CreateFile();
+                        }
+                        else if(command.equals("ConsultFiles"))
+                        {
+
+                        }
+                        else if(command.equals("AmendFiles"))
+                        {
+
+                        }
+                        else if(command.equals("ShowFileInfo"))
+                        {
+
+                        }
+                        else
+                        {
+                            System.out.println("La commande n'existe pas");
+                        }
+                    };
                 }
                 catch(IOException i){
                     System.out.println(i);
@@ -39,4 +69,64 @@ public class Serveur {
             System.out.println(e);
         }
     }
+
+    private boolean CheckProtocol(String line)
+    {
+        if(line.length() >= protocol.length())
+        {
+            String isThisProtocol = line.substring(0,protocol.length());
+            if(isThisProtocol.equals(protocol))
+            {
+                System.out.println("Requête acceptée");
+                return true;
+            }
+            else if(!isThisProtocol.equals(protocol))
+            {
+                System.out.println("Requête refusée");
+            }
+        }
+        return false;
+    }
+
+    public String CheckCommand(String line)
+    {
+        String command = line.substring(protocol.length() + 1,line.length());
+        return command;
+    }
+    public File CreateFile()
+    {
+        System.out.println("Enter file name :");
+        String line = "";
+
+        try
+        {
+            line = in.readUTF();
+            line +=".txt";
+
+            String path ="C:\\Users\\Jonathan\\Desktop\\Labo3Reseau\\LaboReseauCours3\\Serveur\\src\\/Files/" + line;
+            File f = new File(path);
+
+
+            if(f.createNewFile())
+            {
+                System.out.println("File created : " + f.getName());
+                return f;
+            }
+            else
+            {
+                System.out.println("File already exists");
+            }
+        }
+        catch(IOException e)
+        {
+            System.out.println(e);
+        }
+        return null;
+    }
+
+    public void ConsultFiles()
+    {
+
+    }
+
 }
